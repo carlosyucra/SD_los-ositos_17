f7accbc12399559209267473b879718d50b7e394
bcar <bcar@bcar>
2017-02-18 22:49:49 -0500

    eliminacion de prueba

--- src/Menu.java
+++ src/Menu.java
@@ -38,8 +38,6 @@ public class Menu extends JFrame{
 		creador();
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-                
-                new Ventana();
 		
 	}
 	public void cambiarTexto(String t){
